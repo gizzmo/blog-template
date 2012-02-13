6a052c0583444ccf475b830d7e21ef0e6ae020cd
Gizzmo <justgiz@gmail.com>
2012-02-12 16:08:23 -0800

    Massive changes while i was unable to commit them

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
+// Anon wrapper
 (function($){
+
+	// Jquery ready
+	$(function(){
+
+		// Highlight comments via location.hash
+		$(window).bind('hashchange', function(){
+			// start with removing all the highlighting
+			$('.highlighted').removeClass('highlighted');
+
+			// then add the new one
+			if ( location.hash && location.hash.match(/^#comment-\d+$/) ){
+				$(location.hash).addClass('highlighted');
+			}
+		})
+
+
+
+	});
 
 
 
